a36840d2faf3e80d8796cd3b5298a37fb3b30eba
Rohan Shukla <rohan.shukla@gmail.com>
2026-01-30 13:11:29 +0530

    Fix Netlify build: correct imports and Node version

--- adminpanel/src/App.jsx
+++ adminpanel/src/App.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Addfruit from './Pages/Add fruit/Addfruit';
-import Listfruit from './Pages/Listfruit/Listfruit';
-import Order from './Pages/order/order';
+
+// Page components (paths and casing must match exactly on Netlify/Linux)
+import AddFruit from './Pages/Add fruit/AddFruit.jsx';
+import ListFruit from './Pages/Listfruit/ListFruit.jsx';
+import Order from './Pages/order/Order.jsx';
 
 import SIdebar from './Sidebar/SIdebar';
 import Menubar from './Menubar/Menubar';
 
 const App = () => {
   return (
-
     <div className="d-flex" id="wrapper">
-       
-      
-
       <SIdebar />
-
-     
       <div id="page-content-wrapper">
         <Menubar />
         <div className="container-fluid">
           <Routes>
-            <Route path="/add" element={<Addfruit />} />
-            <Route path="/list" element={<Listfruit />} />
+            <Route path="/add" element={<AddFruit />} />
+            <Route path="/list" element={<ListFruit />} />
             <Route path="/order" element={<Order />} />
-            <Route path="/" element={<Listfruit />} />
-            <Route path="/Order" element={<Order />} />
-
-            
+            <Route path="/" element={<ListFruit />} />
           </Routes>
         </div>
       </div>
